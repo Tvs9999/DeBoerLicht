303b894601f16de4efb8d8fec046505362f8cb05
kevinka1 <kevinka1239@gmail.com>
2022-05-31 14:06:56 +0200

    Update inlog.php

--- inlog.php
+++ inlog.php
@@ -12,40 +12,46 @@
     <div id="main">
         <h1>login</h1>
         <form method="post" action="">
-            <input type="text" name="email" class="text" autocomplete="off" required placeholder="emailadress"><br><hr><br>
+            <input type="text" name="email" class="text" autocomplete="off" required placeholder="email"><br><hr><br>
             <input type="wachtwoord" name="wachtwoord" class="text" required placeholder="wachtwoord"><br><hr><br>
             <input type="submit" name="submit" id="sub"> 
       </form>
         <div class="login-button">
-      <a href="bestel(particulier).php"> <button type="submit">particulier klik hier</button></a>
+      
    </div>
     </center>
     </div>
 </body>
 </html>
 <?php
-}
+include 'config.php';
 if (isset($_POST['submit'])) {
-    $emailadress=$_POST['email'];
+    $email=$_POST['email'];
     $wachtwoord=$_POST['wachtwoord'];
       
        
       }
   
-        $sql = "SELECT * FROM `beheerder` WHERE `emailadress` = '".$emailadress."' AND `password` = '".$wachtwoord."'";
-        $result = $conn->query($sql);
+        $sql = "SELECT * FROM `beheerder` WHERE `email` = '".$email."' AND `wachtwoord` = '".$wachtwoord."'";
+        
   
-      if ($result->num_rows > 0) {
-          $_SESSION['emailadress'] = $emailadress;
+      if
+          ($_SESSION['email'] = $email); {
           header("location:Producten.php");
           exit();
-      }
-      else{
-          echo  "<script>alert('emailadress en wachtwoord kloppen niet, Probeer opnieuw')</script>";
-        }
-    }
-    $conn->close();
-    
+          }
+           $conn->close();
+
+        
+
 ?>
+
+        
+       
+        
+    
+   
+    
+
 
 
