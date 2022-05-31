3208ea5b4818e911c97c7c162c60dfebfc475bdb
kevinka1 <kevinka1239@gmail.com>
2022-05-31 13:44:06 +0200

    Update inlog.php

--- inlog.php
+++ inlog.php
@@ -1,59 +1,51 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login.html</title>
+    <link rel="stylesheet" href="DeBoerLicht">
+</head>
+<body>
+ <center>
+    <div id="main">
+        <h1>login</h1>
+        <form method="post" action="">
+            <input type="text" name="email" class="text" autocomplete="off" required placeholder="emailadress"><br><hr><br>
+            <input type="wachtwoord" name="wachtwoord" class="text" required placeholder="wachtwoord"><br><hr><br>
+            <input type="submit" name="submit" id="sub"> 
+      </form>
+        <div class="login-button">
+      <a href="bestel(particulier).php"> <button type="submit">particulier klik hier</button></a>
+   </div>
+    </center>
+    </div>
+</body>
+</html>
 <?php
-include "config.php";
-
-if(isset($_POST['but_submit'])){
-
-    $name = mysqli_real_escape_string($con,$_POST['txt_uname']);
-    $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
-
-    $id = "SELECT id AS countid FROM users WHERE naam='".$uname."' and wachtwoord='".$password."'";
-    $resultid = mysqli_query($con,$id);
-    $rowid = mysqli_fetch_array($resultid);
-    $countid = $rowid['countid'] ?? null;
-
-    if($uname != "" && $password != ""){
-
-        $sql_query = "select count(*) as Count from users where naam='".$uname."' and wachtwoord='".$password."'";
-        $result = mysqli_query($con,$sql_query);
-        $row = mysqli_fetch_array($result);
-        $count = $row['Count'];
-        if ($count > 0){
-            //succes
-            $_SESSION['id'] = $countid;
-            $_SESSION['rol'] = $countrol;
-            header("Location:home.php?username=$uname");
-             exit();   
-        }
-        else {
-            //failed
-            echo "<script>alert('invalid password')</script>";
+}
+if (isset($_POST['submit'])) {
+    $emailadress=$_POST['email'];
+    $wachtwoord=$_POST['wachtwoord'];
+      
+       
+      }
+  
+        $sql = "SELECT * FROM `beheerder` WHERE `emailadress` = '".$emailadress."' AND `password` = '".$wachtwoord."'";
+        $result = $conn->query($sql);
+  
+      if ($result->num_rows > 0) {
+          $_SESSION['emailadress'] = $emailadress;
+          header("location:Producten.php");
+          exit();
+      }
+      else{
+          echo  "<script>alert('emailadress en wachtwoord kloppen niet, Probeer opnieuw')</script>";
         }
     }
-}
+    $conn->close();
+    
 ?>
-<!DOCTYPE html>
-<html lang="NL">
-    <head>
-        <title>Login</title>
-        <link rel="stylesheet" href="inloggenpagina.css">
-    </head>
 
-    <body>
-        <div class="container3">
-            <form method="post" action = "">
-                <div id="div_login">
-                    <h1 class="login">Log in</h1>
-                        <div>
-                          <h2 class="text"> Gebruikersnaam: <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Naam"/> </h2>
-                        </div>
-                        <div>
-                            <h2 class="text"> Wachtwoord: <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Wachtwoord" /> <h2>
-                        </div>
-                         <div>
-                            <input type="submit" value="Log in" name="but_submit" id="but_submit" /> <a href="#"class="lenen" value="Lenen">Lenen</a>
-                        </div>
-                </div>
-            </form>
-        </div>
-    </body>
-</html>
+
