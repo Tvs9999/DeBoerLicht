55b0c71ff67055053306eef4fa0ac7aa06f9c4f3
Yasser30007 <234880@rocfriesepoort.nl>
2022-05-31 14:01:06 +0200

    connection folder

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$db="deboerlicht";
+
+$conn= new mysqli($servername, $username, $password, $db);
+if(!$conn){
+    die("Error on the Connection" . $conn->connect_error);
+}
+?>
--- producten.php
+++ producten.php
@@ -1,1 +1,4 @@
-<?php include"header.php" ?>
+<?php 
+  include "header.php";
+  include "connection.php";
+?>
