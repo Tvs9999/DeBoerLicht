1c53c621535e60c87a5e02828eb6987905e06bed
Tvs9999 <Tvanspanning@gmail.com>
2022-06-11 09:41:19 +0200

    changes

---
+++ ProductTest.php
@@ -0,0 +1,27 @@
+<?php
+include 'connection.php';  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css" integrity="sha384-zIaWifL2YFF1qaDiAo0JFgsmasocJ/rqu7LKYH8CoBEXqGbb9eO+Xi3s6fQhgFWM" crossorigin="anonymous">
+    <link rel="stylesheet" href="DeBoerLicht.css">
+
+
+<body class="">
+    <div class="container">
+        <div class="product-container">
+            <h1>hello</h1>
+        </div> 
+        <div class="sidebar-left">
+            <?php include 'header.php'?>
+        </div>
+</div>
+</body>
+</html>
