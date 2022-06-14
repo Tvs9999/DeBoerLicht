caf5cd965b4dd962e5138eb508b3b1635067c3f0
Yasser30007 <234880@rocfriesepoort.nl>
2022-06-14 14:22:10 +0200

    toevoegen en bewerken modals

---
+++ toevoegenvb.php
@@ -0,0 +1,69 @@
+<?php
+include 'connection.php';
+
+?>
+
+<head>
+  <link rel="stylesheet" href="toevoegen&bewerken.css">
+</head>
+
+<body>
+
+<!-- Trigger/Open The Modal -->
+<button id="toevoegen-btn">Toevoegen</button>
+
+<!-- The Modal -->
+<div id="Toevoegen" class="toevoegen">
+
+  <!-- Modal content -->
+  <div class="toevoegen-content">
+    <span class="close">&times;</span>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      <input type="hidden" name="id"><br>
+      <input type="text" name="productnaam" class="input" placeholder="Product Naam"><br>
+      <input class="input" type="name" name="price" placeholder="Prijs"><br>
+      <input class="input" type="name" name="korting" placeholder="Korting"><br>
+      <input class="input" type="name" name="type" placeholder="Type"><br>
+      <input class="input" type="name" name="voltage" placeholder="Voltage"><br>
+      <input class="input" type="name" name="categorie" placeholder="Categorie"><br>
+      <input class="input" type="name" name="beschikbaarheid" placeholder="Voorraad"><br>
+      <input class="bestand" type="file" name="file1" id="file1"><br>
+      <input class="bestand" type="file" name="file2" id="file2"><br>
+      <input class="upload" type="submit" value="Toevoegen" name="submit">
+    </form>
+  </div>
+
+</div>
+
+
+
+
+<script>
+// Get the modal
+var modal = document.getElementById("Toevoegen");
+
+// Get the button that opens the modal
+var btn = document.getElementById("toevoegen-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+</body>
---
+++ upload.php
@@ -0,0 +1,67 @@
+<?php
+
+include "connection.php";
+
+$id = $_POST['id'];
+$naam = $_POST['productnaam'];
+$prijs = $_POST['price'];
+$korting = $_POST['korting'];
+$type = $_POST['type'];
+$voltage = $_POST['voltage'];
+$categorie = $_POST['categorie'];
+$beschikbaarheid = $_POST['beschikbaarheid'];
+$target_file = $fileDestination . basename($_FILES["file1"]["name"]);
+$target_file2 = $fileDestination2 . basename($_FILES["file2"]["name"]);
+
+
+
+if (isset($_POST['submit'])) {    
+
+    $fileName = $_FILES['file1']['name'];
+    $fileTmpName = $_FILES['file1']['tmp_name'];
+    $fileSize = $_FILES['file1']['size'];
+    $fileError = $_FILES['file1']['error'];
+    $fileType = $_FILES['file1']['type'];
+
+    $fileName2 = $_FILES['file2']['name'];
+    $fileTmpName2 = $_FILES['file2']['tmp_name'];
+    $fileSize2 = $_FILES['file2']['size'];
+    $fileError2 = $_FILES['file2']['error'];
+    $fileType2 = $_FILES['file2']['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $fileExt2 = explode('.', $fileName2);
+    $fileActualExt2 = strtolower(end($fileExt2));
+
+    $allowed = array('jpg', 'jpeg', 'png');
+
+    if (in_array( $fileActualExt, $allowed) && in_array( $fileActualExt2, $allowed)) {
+        if ($fileError === 0 && $fileError2 === 0){
+            if ($fileSize < 1000000 && $fileSize2 < 1000000){
+                $fileNameNew = uniqid().$file1.".".$fileActualExt;
+                $fileDestination = 'upload/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                $fileNameNew2 = uniqid().$file2.".".$fileActualExt2;
+                $fileDestination2 = 'upload/'.$fileNameNew2;
+                move_uploaded_file($fileTmpName2, $fileDestination2);
+
+                $insertsql = "INSERT INTO `producten`(`id`, `naam`, `prijs`, `korting`, `type`, `voltage`, `categorie`, `beschikbaarheid`, `file1`, `file2`) VALUES ('$id','$naam','$prijs','$korting','$type','$voltage','$categorie','$beschikbaarheid .','$target_file','$target_file2')";
+                $insert = $conn->query($insertsql);
+                header("Location: toevoegenvb.php");
+                echo "<script>alert('Product heeft toegevoegd')</script>";
+
+            } else {
+                echo "Uw foto is te groot.";
+            }
+
+        } else {
+            echo "Er is een fout met het uploaden van de foto.";
+        }
+    }
+        
+} else {
+    echo "Er is een fout met het uploaden van de foto.";
+}
+?>
