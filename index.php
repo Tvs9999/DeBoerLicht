fa68de142d934f117902cec00f1c4d6e4de3be9f
kevinka1 <kevinka1239@gmail.com>
2022-06-22 09:03:04 +0200

    new

--- index.php
+++ index.php
@@ -20,28 +20,64 @@ include("connection.php");
                 $query= "SELECT * FROM categorie";
                 $data = mysqli_query($conn,$query);
                 $total = mysqli_num_rows($data);
-                    if($total!=0){
-                        while($result=mysqli_fetch_assoc($data)){ ?>
+                if($total!=0){
+                    while($result=mysqli_fetch_assoc($data)){ ?>
                                     <a href="product.php?categorie=<?php echo $result['naam'];?>">
                                         <div class="categorie">
-                                            <div class="categorie-top">
-                                                <h2 class="categorie-naam"><?php echo $result['naam']; ?></h2>   
+                                            <div class="categorie-links">
+                                                <div class="categorie-top">
+                                                    
+                                                    <h2 class="categorie-naam"><?php echo $result['naam']; ?></h2>   
+                                                </div>
+                                                <div class="categorie-bottom">
+                                                    <?php
+                                                        echo "<img src='UploadImg/".($result['foto'])."' class = 'categorie-foto'>";
+                                                        ?>                            
+                                                </div>   
+                                                
                                             </div>
-                                            <div class="categorie-bottom">
+                    
+                                           <?php $catId = $result['id'] ?>
+                                            
+                
+                                            <div class="categorie-rechts">
+                                               <?php  $sql= "SELECT * FROM producten WHERE catId=$catId order by korting DESC LIMIT 4";
+                                            
+                                               $Kevin = mysqli_query($conn,$sql);
+                                               $totaleDing = mysqli_num_rows($Kevin);	
+
+                                               if($totaleDing!=0){
+                                                while($productName=mysqli_fetch_assoc($Kevin)) { ?>
+                                                <div class="korting-product">
+                                                <h2 class="categorie-naam"><?php echo $productName['korting']; ?></h2>
                                                 <?php
-                                                    echo "<img src='UploadImg/".($result['foto'])."' class = 'categorie-foto'>";
-                                                ?>                            
-                                            </div>   
+                                                        echo "<img src='UploadImg/".($productName['foto'])."' class = 'categorie-foto'>";
+                                                        ?> 
+
+                                                </div>
+                                                    
+                                                
+                                                <?php }
+                                               }
+                                                ?>
+
+
+
+
+
+                                               
+                                            </div>
                                         </div>
                                     </a>
 
 
-                                        
+                            
                                      
 
                         <?php }
                     } ?>  
-                    
+
+
                     
         </div>
         <div class="sidebar-left">
@@ -50,3 +86,4 @@ include("connection.php");
     </div>
 </body>
 </html>
+            
