81303594edf98c7e0b6ce7be8c6b9ced6a946b25
Yasser30007 <234880@rocfriesepoort.nl>
2022-06-29 09:27:46 +0200

    bewerken klaar

--- bewerken.php
+++ bewerken.php
@@ -15,7 +15,7 @@ if ($resultCheck > 0) {
   <div class="toevoegen">
 
     <div class="toevoegen-content">
-      <form action="" method="POST">
+      <form action="" method="POST" enctype="multipart/form-data">
         <input type="hidden" name="id"><br>
         <input class="input" type="text" name="naam" value="<?php echo $_GET["naam"] ?>"><br>
         <input class="input" type="text" name="prijs" value="<?php echo $_GET["prijs"] ?>"><br>
@@ -27,7 +27,7 @@ if ($resultCheck > 0) {
         <input class="input" type="file" name="Foto1" value="<?php echo $_GET["Foto1"] ?>"><br>
         <input class="input" type="file" name="Foto2" value="<?php echo $_GET["Foto2"] ?>"><br>
 
-        <input class="toevoegen-btn" type="submit" value="Update" name="submit">
+        <input class="toevoegen-btn" type="submit" value="Update" name="update-submit">
       </form>
     </div>
   </div>
@@ -36,7 +36,7 @@ if ($resultCheck > 0) {
 </html>
 
 <?php }
-if (isset($_POST['submit'])) {
+if (isset($_POST['update-submit'])) {
   $id = $_GET['id'];
   $naam = $_POST['naam'];
   $prijs = $_POST['prijs'];
@@ -45,10 +45,22 @@ if (isset($_POST['submit'])) {
   $voltage = $_POST['voltage'];
   $catId = $_POST['catId'];
   $voorraad = $_POST['voorraad'];
+  $Foto1 = $_FILES['Foto1'];
+  $Foto2 = $_FILES['Foto2'];
 
-  $query = "UPDATE producten SET naam='$naam',prijs='$prijs',korting='$korting',type='$type',voltage='$voltage',catId='$catId',voorraad='$voorraad' WHERE id='$id'";
+  $updatedfileName = $_FILES['Foto1']['name'];
+  $updatedfileName2 = $_FILES['Foto2']['name'];
+  $fileTmpName = $_FILES['Foto1']['tmp_name'];
+  $fileTmpName2 = $_FILES['Foto2']['tmp_name'];
+  $fileDestination = 'UploadImg/' .$updatedfileName;
+  $fileDestination2 = 'UploadImg/'.$updatedfileName2;
 
-  if(mysqli_query($conn, $query)){
+
+  $query = "UPDATE producten SET naam='$naam',prijs='$prijs',korting='$korting',type='$type',voltage='$voltage',catId='$catId',voorraad='$voorraad',Foto1='$updatedfileName',Foto2='$updatedfileName2' WHERE id='$id'";
+
+  if(mysqli_query($conn, $query)){    
+    move_uploaded_file($fileTmpName, $fileDestination);
+    move_uploaded_file($fileTmpName2, $fileDestination2);
     echo "Gegevens opgeslaan.";
 }else{
   echo "Er is een fout.";
