0b5213a1770e554d29e8ee325ba8bcf4e8f41eaf
Yasser30007 <234880@rocfriesepoort.nl>
2022-07-05 13:27:53 +0200

    .

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 $servername="localhost";
-$username="root";
-$password="";
-$db="deboerlicht";
+$username="deb85590_p42t3";
+$password="F1zJc7Bdt";
+$db="deb85590_p42t3";
 
-$conn = new mysqli($servername, $username, $password, $db);
+$conn= new mysqli($servername, $username, $password, $db);
 if(!$conn){
     die("Error on the Connection" . $conn->connect_error);
 }
